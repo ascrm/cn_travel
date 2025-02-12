36b460666199ed5f2ce22d5f8bf91f58e2dbcd57
ascrm <2339621373@qq.com>
2025-02-12 23:40:03 +0800

    更改

--- src/components/header-navbar/user-info.tsx
+++ src/components/header-navbar/user-info.tsx
@@ -1,6 +1,6 @@
 /** @format */
 
-import UserInfoItem from '@/components/header-navbar/user-info-item'
+import UserOptionItem from '@/components/header-navbar/user-option-item'
 import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined'
 import DescriptionOutlinedIcon from '@mui/icons-material/DescriptionOutlined'
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined'
@@ -8,21 +8,21 @@ import LogoutOutlinedIcon from '@mui/icons-material/LogoutOutlined'
 
 export default function UserInfo() {
   return (
-    <div className="pointer-events-none absolute left-[50%] top-[60px] z-10 ml-[-20px] w-[300px] translate-x-[-50%] rounded-lg border-2 border-solid bg-white p-[10px] opacity-0 shadow-md transition-cartoon group-hover:pointer-events-auto group-hover:opacity-100">
-      <div className={'mt-[60px] text-center text-[2em] font-bold'}>ascrm</div>
+    <div className={'w-[300px]'}>
+      <div className={'mt-[60px] text-center text-[2em] font-bold text-gray-800'}>ascrm</div>
       <div className={'mb-[10px] text-center text-[0.8em] text-gray-500'}>@2339621373@qq.com</div>
-      <UserInfoItem>
+      <UserOptionItem>
         <PersonOutlineOutlinedIcon></PersonOutlineOutlinedIcon>个人中心
-      </UserInfoItem>
-      <UserInfoItem>
+      </UserOptionItem>
+      <UserOptionItem>
         <DescriptionOutlinedIcon></DescriptionOutlinedIcon>我的订单
-      </UserInfoItem>
-      <UserInfoItem>
+      </UserOptionItem>
+      <UserOptionItem>
         <ShoppingCartOutlinedIcon></ShoppingCartOutlinedIcon>我的预定
-      </UserInfoItem>
-      <UserInfoItem>
+      </UserOptionItem>
+      <UserOptionItem>
         <LogoutOutlinedIcon></LogoutOutlinedIcon>退出登录
-      </UserInfoItem>
+      </UserOptionItem>
     </div>
   )
 }
